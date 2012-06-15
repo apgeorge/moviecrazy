8354c7223655220ccdce52fb0c58442b7f373fa8
aroj.george <apgeorge@thoughtworks.com>
2012-06-15 23:42:00 +0100

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviecrazy::Application.config.secret_token = '01b599a307b6fefc5ac35d9b7c9cac45c95fa9f5bd041c623793d0d7d31ea487b7f28a27dd46f7c52bc1fc174e797f97586aa1eb6bb9f77699b09042a54bafd6'
